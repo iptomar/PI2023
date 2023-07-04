b97791b3aceecdb0d92bedbf4bab4333d43226ff
CCovao <carloscovao1999@gmail.com>
2023-07-04 16:20:43 +0100

    alteração da busca
    busca do CORE_VERSION, sendo ainda necessário a busca por service done

--- CORE_VERSION/readFile.php
+++ CORE_VERSION/readFile.php
@@ -5,7 +5,7 @@
     while (!feof($myfile)) {
         $line = fgets($myfile); 
         $row = explode(";", $line);
-        if (count($row) >= 4 && strcmp("SEND_ALGORITHM", $row[4]) == true) {
+        if (count($row) >= 4 && strcmp("CORE_VERSION", $row[4]) == true) {
             $datetime = substr($row[1], 0, 13) . ':00:00.000';
             $data = $datetime;
             $text = $data . "\n";
