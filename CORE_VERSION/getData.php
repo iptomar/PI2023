b81c6cb7a92600f73699291cda445c000e62e1a4
CCovao <carloscovao1999@gmail.com>
2023-07-04 16:07:14 +0100

    ficheiros readFile e GetData
    o ficheiro readFile obtem para um ficheiro o seguinte formato 2022-09-22 12:00:00.000, e o getData devolve o seguinte formato ["2022-09-22 12:00:00.000\n" 87], devolvento tambem a conta da determinada hora no dia especifico

---
+++ CORE_VERSION/getData.php
@@ -0,0 +1,45 @@
+<?php
+    $data = new stdClass();
+    $data->title = 'Count per day';
+    $data->data = array();
+    
+    $label = array();
+    array_push($label, 'days');
+    array_push($label, 'count');
+
+    array_push($data->data, $label);
+
+    $file = fopen("teste.csv", "r");
+    $days = array();
+    while(!feof($file)) {
+        $line = fgets($file); 
+        array_push($days, $line);
+    }
+    fclose($file);
+
+    array_pop($days);
+    $counters = [];
+    // Itera sobre a lista de datas
+    foreach ($days as $day) {
+        // Verifica se a data já existe no array de contadores
+        if (isset($counters[$day])) {
+            // incrementa o contador dos dias
+            $counters[$day]++;
+        } else {
+            // Cria um novo contador para o dia x
+            $counters[$day] = 1;
+        }
+    }
+    $l = array();
+    foreach($counters as $days => $count){
+        array_push($l, $days);
+        array_push($l, $count);
+        array_push($data->data, $l);
+        $l = [];
+    }
+
+    $encodedData = json_encode($data);
+    $encodedDataWithLineBreaks = implode("\n", explode(",", $encodedData));
+
+    echo $encodedDataWithLineBreaks;
+?>
