bbc9c6fce30b671fead2ad8240d5837c1c355b9e
Aluno25076 <aluno25076@ipt.pt>
2023-07-04 14:41:05 +0100

    Envios por dia ja estão feitos, agora vou criar e testar codigo para os meses e para as semanas

--- Send_Algorithm/readFileSend.php
+++ Send_Algorithm/readFileSend.php
@@ -1,27 +1,17 @@
 <?php
-    $myfile = fopen("../teste.csv", "r"); 
-    $data = array();
-    while(!feof($myfile)) {
-        $line = fgets($myfile); 
-        $fields = explode(";", $line);
-        array_push($data, $fields);
-    }
-
-    fclose($myfile);
-
-    print_r($data);
-
-    $text = null;
-
+    $myfile = fopen("../logIPRP.csv", "r"); 
     $filtroFile = fopen("filtroSend.csv", "w");
 
-    foreach ($data as $row) {
-        if(strcmp("SEND_ALGORITHM", $row[4]) == true){
+    while(!feof($myfile)) {
+        $line = fgets($myfile); 
+        $row = explode(";", $line);
+        if(count($row) >= 4 && strcmp("SEND_ALGORITHM", $row[4]) == true){
             $amd = substr($row[1], 0, 10);
             $text = $amd."\n";
             fwrite($filtroFile, $text);
         }
     }
 
+    fclose($myfile);
     fclose($filtroFile);
 ?>
