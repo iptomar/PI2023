c5dec7f38a744c7e023b42c34e904f9b9882c598
Carlos-Henrique-Lopes-da-Silva <aluno21583@ipt.pt>
2023-07-04 10:48:08 +0100

    Optimização do ficheiro de amostra e elaboração de codigo
    Foi elaborado código para a procura, filtragem e isolação de informações necessárias.

--- SEND_ALGORITM/readdata2.php
+++ SEND_ALGORITM/readdata2.php
@@ -3,8 +3,9 @@ $open = fopen("amostra.csv", "r");
 $filteredData = [];
  
 if ($open !== false){
-	while (($commado = fgetcsv($open))!== false){
-		if ($commado[0] >10){
+
+	while (($commado = fgetcsv($open, 0, ';'))!== false){		
+		if (strcmp($commado[3], 'SEND_ALGORITHM      ') ==false){
 			$filteredData[]=$commado;
 		}
 	}
@@ -14,8 +15,16 @@ if ($open !== false){
 
 foreach ($filteredData as $row)
 {
-	$column=$row[0];
+	$column=$row[1];
 	echo $column ;
 	echo "</br>";
+}
+$open2 = fopen("writetest.txt", "w");
+$fsize=filesize("writetest.txt");
+// if ($fsize ==0){
+	foreach($filteredData as $row){
+		//$substri= substr($row[1],0, 10);
+		fwrite($open2, (substr($row[1],0, 4))."; ".(substr($row[1], 5, 2)). "; ".(substr($row[1],8, 2))."; \n");
+	// }
 }
 ?>
