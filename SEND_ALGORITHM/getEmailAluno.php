dea2fa3d9917a9f073d0a733b28fcdd8c9a756cd
aluno25072 <138473041+aluno25072@users.noreply.github.com>
2023-07-06 14:47:28 +0100

    introduz substring até '@'

--- SEND_ALGORITHM/getEmailAluno.php
+++ SEND_ALGORITHM/getEmailAluno.php
@@ -5,17 +5,19 @@ $alunos = array();
 while(!feof($myfile)){
     $line = fgets($myfile);
     $fields = explode(";",$line);
-//Emails aparecem como users em vez de alunoXXXXX...?
+
     if(isset($fields[4]) && isset($fields[3]) && strcmp(trim($fields[3]), "SEND_ALGORITHM") == 0){
-        fwrite($openFile, $fields[4]."\n");   
+        $linha = $fields[4];
+        $substring = strstr($linha,'@',true);
+        echo $substring."<br>";
+        array_push($alunos, $substring);
+        fwrite($openFile, $substring."\n");   
     }
 
 
 }
 fclose($openFile);
 fclose($myfile);
-
-
 
 
 ?>
